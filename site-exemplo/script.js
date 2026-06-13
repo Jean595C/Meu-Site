c5d3d78446d23c7854cc8449878e95a6180b5abb
Jean595C <jeanmagne023@gmail.com>
2026-06-13 15:38:16 -0300

    alterei

---
+++ site-exemplo/script.js
@@ -0,0 +1,7 @@
+const NOVO_TEXTO ="Detalhes: Estou cursando Redes de Computadores pelO PROFITEC em parceria com a Universidade Estadual do Maranhão-UEMA!"
+function mudarConteudo() {
+    let paragrafo = document.getElementById("paragrafo-dinamico");
+    paragrafo.textContent = NOVO_TEXTO;
+}
+let botao = document.getElementById("btn-mudar-texto");
+botao.addEventListener("click", mudarConteudo);
