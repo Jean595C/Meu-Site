62c2218a05904c8c8cc0b70ac50c6b98a56641e3
Jean595C <96322497+Jean595C@users.noreply.github.com>
2026-06-13 15:48:56 -0300

    Update script.js

--- site-exemplo/script.js
+++ site-exemplo/script.js
@@ -1,4 +1,4 @@
-const NOVO_TEXTO ="Detalhes: Estou cursando Redes de Computadores pelO PROFITEC em parceria com a Universidade Estadual do Maranhão-UEMA!"
+const NOVO_TEXTO ="Detalhes: Estou cursando Redes de Computadores pelo PROFITEC em parceria com a Universidade Estadual do Maranhão-UEMA!"
 function mudarConteudo() {
     let paragrafo = document.getElementById("paragrafo-dinamico");
     paragrafo.textContent = NOVO_TEXTO;
